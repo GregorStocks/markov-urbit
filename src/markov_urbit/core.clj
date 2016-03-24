e89c57713c8788b9f05aee5af7ef68033d776166
GregorStocks <gregor@gregorstocks.com>
2016-03-23 20:18:48 -0700

    it works. example

--- src/markov_urbit/core.clj
+++ src/markov_urbit/core.clj
@@ -4,30 +4,34 @@
   (:gen-class))
 
 (defn merge-corpuses [corpuses]
-  (merge-with concat corpuses))
+  (apply merge-with concat corpuses))
+(def lookahead 2)
 
 (def start-paragraph :start)
 (def end-paragraph :end)
 
 (defn paragraph->corpus [paragraph]
   (let [words (concat
-               [start-paragraph]
-               (str/split paragraph "\s+")
+               (repeat lookahead start-paragraph)
+               (str/split paragraph #"\s+")
                [end-paragraph])]
-    (into {} (map (fn [word follower]
-                    [word [follower]])
-                  words
-                  (drop 1 words)))))
+    (loop [offset lookahead
+           corpus {}]
+      (let [[start end] (split-at offset words)
+            preceders (take-last lookahead start)]
+        (if-let [next-word (first end)]
+          (recur (inc offset)
+                 (merge-corpuses [corpus {preceders [next-word]}]))
+          corpus)))))
 
 (defn corpus->paragraph [corpus]
-  (str/join
-   " "
-   (loop [words []
-          current-word start-paragraph]
-     (if (= current-word end-paragraph)
-       words
-       (recur (conj words current-word)
-              (pick (corpus current-word)))))))
+  (let [words (loop [words (into [] (repeat lookahead start-paragraph))]
+                (let [next-word (rand-nth (corpus (take-last lookahead words)))]
+                  (if (or (= next-word end-paragraph)
+                          (> (count words) 1000))
+                    words
+                    (recur (conj words next-word)))))]
+    (str/join " " (drop lookahead words))))
 
 (def filenames ["0-intro"
                 "1-nock"
@@ -46,4 +50,4 @@
   "I don't do a whole lot ... yet."
   [& args]
   (let [corpus (merge-corpuses (map filename->corpus filenames))]
-    (str/join "\n\n" (repeat 20 #(corpus->paragraph corpus)))))
+    (println (str/join "\n\n" (repeatedly 20 #(corpus->paragraph corpus))))))
